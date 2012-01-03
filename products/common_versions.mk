748d57e68c21c2b028e8c2b2dbfdc9cd3af53ffe
Deodexed <Deodexed@gmail.com>
2012-01-02 22:40:46 -0500

    add ro.mod

---
+++ products/common_versions.mk
@@ -0,0 +1,7 @@
+# Version information used on all builds
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+
+# Rom Manager properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=$(TARGET_PRODUCT)_build-1 \
+    ro.rommanager.developerid=ModAGnex
